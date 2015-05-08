ab40181b72baa1ad2300d2ec60530fa9bc912457
trikk <trikk@users.noreply.github.com>
2015-05-08 16:03:35 +0200

    Update formular.php

--- PHP/formular.php
+++ PHP/formular.php
@@ -1,4 +1,24 @@
+Formularaufbau
 <?php
+if isset action && action==xyz
+	$error = array();
+	if ( email != true ) { $error[] = 'Email falsch'; }
+	if ( name != true ) { $error[] = 'Name falsch'; }
+	if ( text != true ) { $error[] = 'Text falsch'; }
+	if count($error) == 0
+		...mail / db eintrag...
+		header location
+		exit;
+
+/** /
+form method=post/get action=ziel
+input hidden name=action value=xyz
+input submit
+/**/
+
+
+// kurzschreibweise
+
 $_GET['inhalt'] = isset($_GET['inhalt']) ? $_GET['inhalt'] : '';
 
 if( isset($_GET['action']) && $_GET['action'] == 'event' ){
